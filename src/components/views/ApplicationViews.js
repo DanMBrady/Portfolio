6741c7ab0edf1dab2a885c172f66a29cfcaecad0
Dan Brady <dannymb360@gmail.com>
2023-09-01 18:28:09 -0400

    logos

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -2,6 +2,8 @@ import { Outlet, Route, Routes } from "react-router-dom"
 import { About } from "../pages/About"
 import { Conatct } from "../pages/Contact"
 import { Projects } from "../pages/Projects"
+import { Pop } from "../pages/Pop"
+import { Flash } from "../pages/Flash"
 
 export const ApplicationViews =()=>{
     return (
@@ -10,6 +12,8 @@ export const ApplicationViews =()=>{
 
             <Route path ="/projects" element={ <Projects/>}></Route>
             <Route path ="/contact" element={ <Conatct/>}></Route>
+            <Route path ="/pop" element={ <Pop/>}></Route>
+            <Route path ="/flash" element={ <Flash/>}></Route>
         </Routes>
     )
 }
