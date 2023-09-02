902281416df5997af1cea5653dea0efa4188d749
Dan Brady <dannymb360@gmail.com>
2023-09-02 19:19:24 -0400

    videos

--- src/components/pages/Flash.js
+++ src/components/pages/Flash.js
@@ -1,6 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 export const Flash =()=>{
     return <div>
         <h1>Flash Forward</h1>
+        <div>
+            <Container>
+                <div className='w-50 p-3 ratio ratio-16x9'>
+                    <iframe src="https://www.youtube.com/embed/pOhgKsjsB9k?si=1rDrYAAVNfXw62zl"
+                    title="YouTube video Player"
+                    allowFullScreen></iframe>
+                </div>
+            </Container>
+       
+        </div>
     </div>
 }
--- src/components/pages/Pop.js
+++ src/components/pages/Pop.js
@@ -1,6 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 export const Pop =()=>{
     return <div>
         <h1>Pop Quiz</h1>
+        
+        <div>
+            <Container>
+                <div className='w-50 p-3 ratio ratio-16x9'>
+                    <iframe src="https://www.youtube.com/embed/pyXNMR1orhE?si=IZ8DEapk3s6BkOT6"
+                    title="YouTube video Player"
+                    allowFullScreen></iframe>
+                </div>
+            </Container>
+       
+        </div>
     </div>
 }
