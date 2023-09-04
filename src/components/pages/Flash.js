461c889ecf18851eb2a5792a25079018019bbfbb
Dan Brady <dannymb360@gmail.com>
2023-09-04 12:28:59 -0400

    proj-resp

--- src/components/pages/Flash.js
+++ src/components/pages/Flash.js
@@ -2,17 +2,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 export const Flash =()=>{
-    return <div>
+    return <div className='indProjectDiv'>
+        <div className='webName'>
         <h1>Flash Forward</h1>
-        <div>
-            <Container>
-                <div className='w-50 p-3 ratio ratio-16x9'>
+        <section>Flash Forward is an app that allows users to create comic book libraries as well as review comics and join book clubs</section>
+        </div>
+        <div className='vidCont'>
+                 <div className='w-50 p-3 ratio ratio-16x9'>
                     <iframe src="https://www.youtube.com/embed/pOhgKsjsB9k?si=1rDrYAAVNfXw62zl"
                     title="YouTube video Player"
                     allowFullScreen></iframe>
                 </div>
-            </Container>
-       
-        </div>
+            </div>
+            <div className='techUsed'>
+                <h3>Technologies Used</h3>
+                <ul>
+                    <li>React</li>
+                    <li>Json Server</li>
+                </ul>
+            </div>
     </div>
 }
+
